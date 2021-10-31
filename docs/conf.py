f7d6f1ef7938d4bf7127eea3aef36b6ffe6dd127
Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>
2021-10-30 22:15:42 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ html_theme_path = ["../_env/Lib/site-packages"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
 pygments_style = 'sphinx'
 highlight_language = 'c++'
