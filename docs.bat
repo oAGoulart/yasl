a39e546d352f4c695b2534e0547268554d29ecb9
Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>
2021-10-30 21:46:43 -0300

    Fix docs build script

--- docs.bat
+++ docs.bat
@@ -4,6 +4,9 @@ pushd %~dp0
 
 REM Command file for building documentation
 
+curl --output doxygen.zip https://www.doxygen.nl/files/doxygen-1.9.2.windows.x64.bin.zip
+tar -x -k -f doxygen.zip
+
 py -m pip install --user virtualenv
 py -m venv _env
 
